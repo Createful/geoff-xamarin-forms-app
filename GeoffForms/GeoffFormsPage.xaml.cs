b4af7a5ca3a43c0aa16cc52dbe81cf064934d2b4
Steve Stanton <steve.stanton@createful.com>
2016-08-04 14:02:02 +0100

    Added the Geoff Image

--- GeoffForms/GeoffFormsPage.xaml.cs
+++ GeoffForms/GeoffFormsPage.xaml.cs
@@ -6,27 +6,40 @@ namespace GeoffForms
 {
 	public partial class GeoffFormsPage : ContentPage
 	{
+
+		Image image;
+
 		public GeoffFormsPage()
 		{
 			InitializeComponent();
+
+			image = this.FindByName<Image>("geoffImage");
+
 		}
 
 		public void OnAskButtonClick(object sender, EventArgs e)
 		{
 			//Console.WriteLine("You clicked this button");
 			OnMagic();
+
 		}
 
 		async void OnMagic() {
+
+			image.BackgroundColor = Color.Blue;
 
 			if (await DisplayAlert(
 				"Welcome",
 				"Would you like to experience the magical powers of Geoff?",
 				"Yes",
 				"No"
-			)) {
+			))
+			{
 				//Console.WriteLine("Magic happens here");
 
+			}
+			else {
+				await DisplayAlert("Bye!","Bye!","Bye!");
 			}
 		}
 	}
