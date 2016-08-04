2b7c15937d00919ee7e502f63c461d8f63ea9ffe
Steve Stanton <steve.stanton@createful.com>
2016-08-04 16:20:43 +0100

    Coded Geoff Decision

--- GeoffForms/GeoffFormsPage.xaml.cs
+++ GeoffForms/GeoffFormsPage.xaml.cs
@@ -9,38 +9,86 @@ namespace GeoffForms
 
 		Image image;
 
+		ContentView noLabel;
+		ContentView yesLabel;
+
+		Color neutralBackground = Color.FromRgb(100,100,100);
+		Color highlightedBackground = Color.FromRgb(255,255,200);
+
+		Random random;
+
 		public GeoffFormsPage()
 		{
 			InitializeComponent();
 
 			image = this.FindByName<Image>("geoffImage");
+			noLabel = this.FindByName<ContentView>("no");
+			yesLabel = this.FindByName<ContentView>("yes");
 
+			random = new Random();
+
+			Reset();
+		}
+
+		void Reset()
+		{
+			yesLabel.BackgroundColor = neutralBackground;
+			noLabel.BackgroundColor = neutralBackground;
+
+			image.BackgroundColor = Color.Black;
+			image.Source = "resting.png";
 		}
 
 		public void OnAskButtonClick(object sender, EventArgs e)
 		{
-			//Console.WriteLine("You clicked this button");
-			OnMagic();
-
+			DoMagic();
 		}
 
-		async void OnMagic() {
+		async void DoMagic() {
 
-			image.BackgroundColor = Color.Blue;
-
-			if (await DisplayAlert(
-				"Welcome",
-				"Would you like to experience the magical powers of Geoff?",
-				"Yes",
-				"No"
-			))
+			Reset();
+			for (int i = 0; i < 10; i++)
 			{
-				//Console.WriteLine("Magic happens here");
+				await Task.Delay(100);
+				image.BackgroundColor = Color.FromRgb(random.Next(256), random.Next(256), random.Next(256));
+			}
 
+			image.BackgroundColor = Color.Black;
+
+			int result = random.Next(0, 2);
+			switch (result) {
+				case 0:
+					SelectYes();
+					break;
+				case 1:
+					SelectNo();
+					break;
 			}
-			else {
-				await DisplayAlert("Bye!","Bye!","Bye!");
-			}
+
+			await Task.Delay(10 * 1000);
+			Reset();
+		}
+
+		void SelectYes() {
+			Highlight(yesLabel, "left_point.png");
+		}
+
+		void SelectNo()
+		{
+			Highlight(noLabel,"right_point.png");
+		}
+
+		void Highlight(ContentView view, String imageSource) {
+			
+			view.BackgroundColor = highlightedBackground;
+			image.Source = imageSource;		
+		}
+
+		void Unhighlight(ContentView view, String imageSource)
+		{
+
+			view.BackgroundColor = neutralBackground;
+
 		}
 	}
 }
